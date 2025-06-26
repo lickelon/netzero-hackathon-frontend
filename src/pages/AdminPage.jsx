bafb412f008e24221b2b3a23ea8bf8fc0b85503c
duram21 <cubeenjoy@naver.com>
2025-06-27 01:54:53 +0900

    feat : admin page design

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -17,11 +17,36 @@ const PageWrap = styled.div`
   height: 100vh;
 `;
 
+const VoteWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin-top: 40px;
+  
+  h{
+    font-weight: bold;
+    font-size: 23px;
+  }
+
+`
+
+const VoteGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+  padding: 10px;
+`;
+
+const CreatePage = styled.button`
+  font-size : 
+`; 
+
+
 export default function SurveyPage() {
   const [searchParams] = useSearchParams();
   const room_id = searchParams.get("room_id") || null;
   const [room, setRoom] = useState("");
-  const [rooms, setRooms] = useState([]);
+  const [votes, setVotes] = useState([]);
   useEffect(() => {
     if (room_id == null) return; //TODO: show room create page
     getRoom(room_id)
@@ -37,7 +62,7 @@ export default function SurveyPage() {
     getRooms(room_id).then((result) => {
       if (result) {
         console.log("투표 데이터 있음:", result);
-        setRooms(result);
+        setVotes(result);
       } else {
         console.log("투표 데이터 없음 또는 요청 실패");
       }
@@ -46,19 +71,50 @@ export default function SurveyPage() {
     });
   }, [room_id]);
 
+  const [currentPage, setCurrentPage] = useState(0);
+  const ITEMS_PER_PAGE = 3;
 
+  const startIndex = currentPage * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+  const currentVotes = votes.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(votes.length / ITEMS_PER_PAGE);
 
 
 
   return (
     <PageWrap>
       <h1>관리자 페이지</h1>
-      {room ? <RoomInfo room={room}></RoomInfo> : <div>create page</div>}
+      {room ? <RoomInfo room={room}></RoomInfo> : <button>create page</button>}
       
+      
+      {room ?
+      <VoteWrap>
+        <h>해당 칸의 설문 정보</h>
+        <VoteGrid>
+          {currentVotes.map((cur) => (
+            <Votes value={cur} key={cur.voteId} />
+          ))}
+        </VoteGrid>
 
-      {rooms.map((cur) => {
-        return <Votes value={cur} key={cur.voteId}/>
-      })}
+        <div style={{ display: "flex", gap: "10px", justifyContent: "center", marginTop: "10px" }}>
+          <button
+            onClick={() => setCurrentPage((p) => Math.max(p - 1, 0))}
+            disabled={currentPage === 0}
+          >
+            이전
+          </button>
+
+          <span>{currentPage + 1} / {totalPages}</span>
+
+          <button
+            onClick={() => setCurrentPage((p) => Math.min(p + 1, totalPages - 1))}
+            disabled={currentPage === totalPages - 1}
+          >
+            다음
+          </button>
+        </div>
+      </VoteWrap> : null}
     </PageWrap>
   )
 }
