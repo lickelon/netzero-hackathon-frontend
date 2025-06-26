5cf1e8764f8cfc09910291b6716c4f084b11c886
lickelon <jindoman0@naver.com>
2025-06-26 15:50:08 +0900

    feat: implement mock API functions

---
+++ src/services/Api.js
@@ -0,0 +1,42 @@
+/*
+ * Vote
+ * pos_x: float
+ * pos_y: float
+ * enter_time: string
+ * vote_time: string
+ * score: int
+ */
+export function sendVote(vote) {
+  //TODO: send vote data to backend
+  console.log(vote);
+}
+export function getLatestVoteTime(uuid) {
+  //TODO: get latest vote time from backend
+  let date = new Date();
+  return date;
+}
+/*
+ * Room
+ * name: string
+ * width: float
+ * height: float
+ * depth: float
+ * QR: [(float,float), ...]
+ * AC: [(float,float), ...]
+ */
+
+export function getRoom(room_id) {
+  //TODO: get Room data from backend
+  let room = {};
+  room['name'] = 'my room';
+  room['width'] = 30.0;
+  room['height'] = 6.0;
+  room['depth'] = 1.0;
+  room['QR'] = [];
+  room['AC'] = [];
+  room['QR'].push([15, 0]);
+  room['AC'].push([30, 6]);
+
+  console.log(room);
+  return room;
+}
